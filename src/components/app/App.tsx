50413df05efbec243bc8587c261699bfa662b4f9
Manu <le.marques.emanuel@gmail.com>
2023-07-07 17:29:07 +0200

    added new component

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -5,17 +5,18 @@ import Projets from "../projets";
 import Skills from "../skills";
 import Experience from "../experience";
 import Formations from "../formations";
+import Prestation from "../prestation";
 import Contact from "../contact";
 import Footer from "../footer";
 import ScrollToTop from "../scrollToTop";
 import Arrow from "../arrow";
 
 //Images
-import photo from '../../../src/assets/photo.jpg';
+import photo from '../../../src/assets/Moi.png';
 
 //React
-// import { Link } from "react-router-dom";
 import { Link as ScrollLink, Element } from 'react-scroll';
+
 
 export default function App() {
 
@@ -24,31 +25,33 @@ export default function App() {
     <div className="App">
       <div className="home">
         <div className='home-container'>
-          <div className="home-row">
-            <div className='home-resume'>
-              <div className="home-me">
-                <h2 className='home-presentation'>Bonjour, je m'appelle <span className="presentation-word">Emanuel</span></h2>
-                <h2 className='home-presentation'>Je suis un <span className="presentation-word">Développeur Web</span> qui aspire constamment à monter en compétences et à relever de nouveaux défis. </h2>
-              </div>
-              <img className='home-img' src={photo} alt="moi" />
-            </div>
             <div className="home-menu">
-              <div className="home-left">
                 <ScrollLink className="home-experience" to="experience" spy={true} smooth={true} offset={-50} duration={500}>
                   <div className="home-category">Expériences</div>
                 </ScrollLink>
                 <ScrollLink className="home-skills" to="skills" spy={true} smooth={true} offset={-50} duration={500}>
                   <div className="home-category">Compétences</div>
                 </ScrollLink>
-              </div>
-              <div className="home-right">
+                <ScrollLink className="home-prestation" to="prestation" spy={true} smooth={true} offset={-50} duration={500}>
+                    <div className="home-category">Prestations</div>
+                  </ScrollLink>
                 <ScrollLink className="home-projets" to="projets" spy={true} smooth={true} offset={-50} duration={500}>
                   <div className="home-category">Projets</div>
                 </ScrollLink>
                 <ScrollLink className="home-formations" to="formations" spy={true} smooth={true} offset={-50} duration={500}>
                   <div className="home-category">Formations</div>
                 </ScrollLink>
-              </div>
+                  <ScrollLink className="home-contact" to="contact" spy={true} smooth={true} offset={-50} duration={500}>
+                    <div className="home-category">Contact</div>
+                  </ScrollLink>
+            </div>
+            <div className='home-resume'>
+              <div className="home-image">
+                <img className='home-img' src={photo} alt="moi" />
+                </div>
+              <div className="home-me">
+                <h2 className='home-presentation'>Bonjour, je m'appelle <span className="presentation-word">Emanuel</span></h2>
+                <h2 className='home-presentation'>Je suis un <span className="presentation-word">Développeur Web</span> qui aspire constamment à monter en compétences et à relever de nouveaux défis. </h2>
             </div>
           </div>
         </div>
@@ -66,8 +69,13 @@ export default function App() {
       <Element name="formations">
         <Formations />
       </Element>
+      <Element name="prestation">
+        <Prestation />
+      </Element>
       <Arrow />
+      <Element name="contact">
         <Contact />
+      </Element>
       <Footer />
     </div>
   );
---
+++ src/components/prestation/index.tsx
@@ -0,0 +1,49 @@
+import "./styles.css";
+
+
+export default function Prestation() {
+  return (
+    <div className="prestation">
+      <div className="prestation-container">
+        <div className="prestation-row">
+          <div className="prestation-left">
+            <div className="prestation-left-container">
+              <h2 className="prestation-title">Prestations</h2>
+              <div className="prestation-text">
+                <p className="prestation-p">
+                  Je vous propose mes services pour la création de votre site web.
+                </p>
+                <p className="prestation-p">
+                  Je peux également vous aider à maintenir votre site web existant.
+                </p>
+                <p className="prestation-p">
+                  Je peux vous aider à créer votre site web en utilisant les technologies suivantes:
+                </p>
+                <ul className="prestation-list">
+                  <li className="prestation-li">HTML5</li>
+                  <li className="prestation-li">CSS3</li>
+                  <li className="prestation-li">Javascript</li>
+                  <li className="prestation-li">React</li>
+                  <li className="prestation-li">Node.js</li>
+                  <li className="prestation-li">Express.js</li>
+                  <li className="prestation-li">MongoDB</li>
+                  <li className="prestation-li">MySQL</li>
+                </ul>
+                <p className="prestation-p">
+                  Je peux également vous aider à créer votre site web en utilisant les CMS suivants:
+                </p>
+                <ul className="prestation-list">
+                  <li className="prestation-li">Wordpress</li>
+                  <li className="prestation-li">Prestashop</li>
+                </ul>
+                <p className="prestation-p">
+                  Je peux vous aider à créer votre site web en utilisant les technologies suivantes:
+                </p>
+                </div>
+                </div>
+                </div>
+                </div>
+                </div>
+                </div>
+    );
+}
