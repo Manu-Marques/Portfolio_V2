e386c51773660d4a6cbbea4549e63628235d0e0c
Manu <le.marques.emanuel@gmail.com>
2023-07-11 11:56:57 +0200

    css added

--- src/components/prestation/index.tsx
+++ src/components/prestation/index.tsx
@@ -4,49 +4,34 @@ import "./styles.css";
 export default function Prestation() {
   return (
     <div className="prestation-main">
-              <h2 className="prestation-title">Prestations</h2>
-              <div className="prestation-table">
-              
-      <div className="prestation-container">
-        <div className="prestation-row">
-          <div className="prestation-left">
-            <div className="prestation-left-container">
-              <div className="prestation-text">
-                <p className="prestation-p">
-                  Je vous propose mes services pour la création de votre site web.
-                </p>
-                <p className="prestation-p">
-                  Je peux également vous aider à maintenir votre site web existant.
-                </p>
-                <p className="prestation-p">
-                  Je peux vous aider à créer votre site web en utilisant les technologies suivantes:
-                </p>
-                <ul className="prestation-list">
-                  <li className="prestation-item">HTML5</li>
-                  <li className="prestation-item">CSS3</li>
-                  <li className="prestation-item">Javascript</li>
-                  <li className="prestation-item">React</li>
-                  <li className="prestation-item">Node.js</li>
-                  <li className="prestation-item">Express.js</li>
-                  <li className="prestation-item">MongoDB</li>
-                  <li className="prestation-item">MySQL</li>
-                </ul>
-                <p className="prestation-p">
-                  Je peux également vous aider à créer votre site web en utilisant les CMS suivants:
-                </p>
-                <ul className="prestation-list">
-                  <li className="prestation-li">Wordpress</li>
-                  <li className="prestation-li">Prestashop</li>
-                </ul>
-                <p className="prestation-p">
-                  Je peux vous aider à créer votre site web en utilisant les technologies suivantes:
-                </p>
-                </div>
-                </div>
-                </div>
-                </div>
-                </div>
-                </div>
-                </div>
-    );
+      <h1 className="prestation-title">Prestations</h1>
+      <div className="prestation-table">
+
+        <div className="prestation-container">
+          <div className="prestation-web">
+            <h2 className="prestation-subtitle">Pourquoi faire un site internet ?</h2>
+            <p className="prestation-web-text">Avoir un site internet aujourd'hui est crucial pour une entreprise, car il permet d'être présent en ligne en permanence, d'atteindre un public large et de promouvoir efficacement les produits ou services. De plus, un site web offre la possibilité d'interagir directement avec les clients, favorisant ainsi la croissance de l'entreprise.</p>
+          </div>
+            <div className="prestation-whyme">
+              <h2 className="prestation-whyme-title">Pourquoi me choisir ?</h2>
+              <p className="prestation-whyme-text">Je suis un développeur web à l'écoute de vos besoins et je vous accompagne tout au long de votre projet. Je m'engage à établir une relation de confiance avec les clients, en offrant un service transparent</p>
+            </div>
+          <div className="prestation-services">
+            <div className="prestation-design">
+              <h2 className="prestation-design-title">Design</h2>
+              <p className="prestation-design-text">Je m'adapte à vos besoins pour créer un site web qui vous ressemble. Je vous propose un design moderne et épuré, qui s'adapte à tous les supports (ordinateur, tablette, mobile).</p>
+            </div>
+            <div className="prestation-performances">
+              <h2 className="prestation-performances-title">Performance</h2>
+              <p className="prestation-performances-text">Je m'engage à vous fournir un site web performant et optimisé, afin d'offrir une expérience utilisateur de qualité. Je vous propose également un référencement naturel (SEO) pour améliorer la visibilité de votre site web.</p>
+            </div>
+            <div className="prestation-follow">
+              <h2 className="prestation-follow-title">Accompagnement</h2>
+              <p className="prestation-follow-text">Je vous accompagne tout au long de votre projet, de la conception à la mise en ligne de votre site internet. Je vous propose également un accompagnement une fois votre site web en ligne afin de le mettre régulierement à jour.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
