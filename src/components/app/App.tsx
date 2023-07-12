993fc3b08c0438a7813fb5711ca35d5869113954
Manu <le.marques.emanuel@gmail.com>
2023-07-12 20:17:24 +0200

    hamburger nav added

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -16,42 +16,49 @@ import photo from '../../../src/assets/Moi.jpg';
 
 //React
 import { Link as ScrollLink, Element } from 'react-scroll';
+import { useState, useEffect } from "react";
 
 
 export default function App() {
 
+  const [isMobile, setIsMobile] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMobile(!isMobile);
+  };
 
   return (
     <div className="App">
       <div className="home">
         <div className='home-container'>
-            <div className="home-menu">
-                <ScrollLink className="home-experience" to="experience" spy={true} smooth={true} offset={-50} duration={500}>
-                  <div className="home-category">Expériences</div>
-                </ScrollLink>
-                <ScrollLink className="home-skills" to="skills" spy={true} smooth={true} offset={-50} duration={500}>
-                  <div className="home-category">Compétences</div>
-                </ScrollLink>
-                <ScrollLink className="home-prestation" to="prestation" spy={true} smooth={true} offset={-50} duration={500}>
-                    <div className="home-category">Prestations</div>
-                  </ScrollLink>
-                <ScrollLink className="home-projets" to="projets" spy={true} smooth={true} offset={-50} duration={500}>
-                  <div className="home-category">Projets</div>
-                </ScrollLink>
-                <ScrollLink className="home-formations" to="formations" spy={true} smooth={true} offset={-50} duration={500}>
-                  <div className="home-category">Formations</div>
-                </ScrollLink>
-                  <ScrollLink className="home-contact" to="contact" spy={true} smooth={true} offset={-50} duration={500}>
-                    <div className="home-category">Contact</div>
-                  </ScrollLink>
+            <button className="hamburger-btn" onClick={toggleMenu}></button>
+          <div className={`home-items ${isMobile ? 'menu-open' : ''}`}>
+            <ScrollLink className="home-experience" to="experience" spy={true} smooth={true} offset={-50} duration={500}>
+              <div className="home-category">Expériences</div>
+            </ScrollLink>
+            <ScrollLink className="home-skills" to="skills" spy={true} smooth={true} offset={-50} duration={500}>
+              <div className="home-category">Compétences</div>
+            </ScrollLink>
+            <ScrollLink className="home-prestation" to="prestation" spy={true} smooth={true} offset={-50} duration={500}>
+              <div className="home-category">Prestations</div>
+            </ScrollLink>
+            <ScrollLink className="home-projets" to="projets" spy={true} smooth={true} offset={-50} duration={500}>
+              <div className="home-category">Projets</div>
+            </ScrollLink>
+            <ScrollLink className="home-formations" to="formations" spy={true} smooth={true} offset={-50} duration={500}>
+              <div className="home-category">Formations</div>
+            </ScrollLink>
+            <ScrollLink className="home-contact" to="contact" spy={true} smooth={true} offset={-50} duration={500}>
+              <div className="home-category">Contact</div>
+            </ScrollLink>
+          </div>
+          <div className='home-resume'>
+            <div className="home-image">
+              <img className='home-img' src={photo} alt="moi" />
             </div>
-            <div className='home-resume'>
-              <div className="home-image">
-                <img className='home-img' src={photo} alt="moi" />
-                </div>
-              <div className="home-me">
-                <h2 className='home-presentation'>Bonjour, je m'appelle <span className="presentation-word">Emanuel</span></h2>
-                <h2 className='home-presentation'>Je suis un <span className="presentation-word">Développeur Web</span> qui aspire constamment à monter en compétences et à relever de nouveaux défis. </h2>
+            <div className="home-me">
+              <h2 className='home-presentation'>Bonjour, je m'appelle <span className="presentation-word">Emanuel</span></h2>
+              <h2 className='home-presentation'>Je suis un <span className="presentation-word">Développeur Web</span> qui aspire constamment à monter en compétences et à relever de nouveaux défis. </h2>
             </div>
           </div>
         </div>
