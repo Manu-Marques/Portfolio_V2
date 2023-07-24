efb26751e68bc16d0bedfacef58a2732d62361b6
Manu <le.marques.emanuel@gmail.com>
2023-07-24 17:57:58 +0200

    animation added to loader

--- src/components/loader/index.tsx
+++ src/components/loader/index.tsx
@@ -1,9 +1,11 @@
 import "./styles.css";
+import logo from '../../../src/assets/logo.png';
 
 export default function Loader() {
     return (
-        <div id="loader">
-        <div className="loader-animation"></div>
+        <div className="loader">
+        <img className='loader-logo' src={logo} alt="logo" />
+        <p className="loader-text">Loading...</p>
       </div>
     )
 }
